f28d83769b3e9b16ade58ec5488a06fe34005037
cristianb84 <cristian.bengescu@yahoo.com>
2024-01-12 11:20:59 +0200

    Gen String update.

--- gen_sring.sh
+++ gen_sring.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-length=$1
-
-if [ -z "$length" ]; then
-  echo "Error: Length argument is missing."
-  echo "Usage: $0 <length>"
+usage() {
+  echo "Usage: $0 <length> [-l | -d]"
+  echo "  -l: Generate a string with only letters (A-Za-z)."
+  echo "  -d: Generate a string with only digits (0-9)."
   exit 1
+}
+
+if [ $# -eq 0 ]; then
+  usage
 fi
 
-tr -dc 'A-Za-z0-9' < /dev/urandom | head -c $length ; echo
+length=$1
+shift
+charset="A-Za-z0-9"
+
+while getopts "ld" opt; do
+  case $opt in
+    l)
+      charset="A-Za-z"
+      ;;
+    d)
+      charset="0-9"
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+tr -dc "$charset" < /dev/urandom | head -c $length ; echo
